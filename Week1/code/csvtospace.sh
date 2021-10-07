a3453e71153eee93cb14a40596ecf4d57790b1fb
atn21 <an.nguyen21@imperial.ac.uk>
2021-10-07 12:03:58 +0100

    initial commit

---
+++ Week1/code/csvtospace.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Author: An an.nguyen21@imperial.ac.uk
+# Script: csvtospace.sh
+# Desc: substitute commas in the files with space
+
+# Save output as .txt file
+# Arguments: 1 > space delimited file
+# Date: Oct 2021
+
+if [ ! -z "$1" ] 
+then echo "Creating a space delimited version of $1 ..."
+    cat ../data/$1.csv | tr -s "," " " >> $1.txt
+    mv "$1.txt" ../results
+    echo "Finish!"
+else 
+    echo "Null input"
+fi
+exit
