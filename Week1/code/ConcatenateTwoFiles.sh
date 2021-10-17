6c095962d23a27ea9627d4369d79c1c028e5e601
atn21 <an.nguyen21@imperial.ac.uk>
2021-10-17 10:58:39 +0100

    Final changes

--- Week1/code/ConcatenateTwoFiles.sh
+++ Week1/code/ConcatenateTwoFiles.sh
@@ -10,18 +10,19 @@ if [ -z "$1" ] || [ -z "$2" ]
 then 
     echo 'Missing inputs please enter 2 file names'
     exit 0 
+else
+    #combine two files into a new file named plzwork
+    touch merged 
+    cat $1 > MERGED
+    cat $2 >> MERGED
+
+    #print content of merged file
+    echo "Merged file saved as MERGED in results/ and is shown below"
+    cat MERGED
+
+    #save the merged file in results/
+    mv MERGED ../results
+
 fi
-
-#combine two files into a new file named plzwork
-touch merged 
-cat $1 > MERGED
-cat $2 >> MERGED
-
-#print content of merged file
-echo "Merged file saved as MERGED in results/ and is shown below"
-cat MERGED
-
-#save the merged file in results/
-mv MERGED ../results
 
 exit
